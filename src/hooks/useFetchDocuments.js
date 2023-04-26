272d1ce26c47affe0331b836bd6613816e4c6885
Yago Hilario <gityago@gmail.com>
2023-04-26 11:35:18 -0300

    finalizado a pagina do post e começando a pagina do dashboard

--- src/hooks/useFetchDocuments.js
+++ src/hooks/useFetchDocuments.js
@@ -26,6 +26,8 @@ export const useFetchDocuments = (docCollection, search = null, uid = null) => {
 
                 if(search) {
                     q = await query(collectionRef, where("tagsArray", "array-contains", search), orderBy("createdAt", "desc"))
+                } else if (uid) {
+
                 } else {
                     q = await query(collectionRef, orderBy("createdAt", "desc"))
                 }
@@ -52,7 +54,7 @@ export const useFetchDocuments = (docCollection, search = null, uid = null) => {
 
         loadData()
 
-    }, [docCollection, search, uid, cancelled])
+    }, [docCollection, documents, search, uid, cancelled])
 
     useEffect(() => {
         return () => setCancelled(true)
--- src/pages/Post/Post.js
+++ src/pages/Post/Post.js
@@ -2,16 +2,32 @@ import styles from './Post.module.css'
 
 // hooks
 import { useParams } from 'react-router-dom'
+import { useFetchDocument } from '../../hooks/useFetchDocument'
 
 
 const Post = () => {
 
     const { id } = useParams()
-  return (
-    <div>
-        <h1>Post</h1>
-    </div>
-  )
+    const {document: post, loading} = useFetchDocument("posts", id)
+
+    return (
+        <div className={styles.post_container}>
+            {loading && <p>Carregando post...</p>}
+            {post && (
+                <div className={styles.img_control}>
+                    <h1>{post.title}</h1>
+                    <img src={post.image} alt={post.title} />
+                    <p>{post.body}</p>
+                    <h3>Este post trata sobre:</h3>
+                    <div className={styles.tags}>
+                        {post.tagsArray.map((tag) => (
+                            <p key={tag}><span>#</span>{tag}</p>
+                        ))}
+                    </div>
+                </div>
+            )}
+        </div>
+    )
 }
 
 export default Post
